efb2c9fc9675473ffb20870e32a6c0c7364990d4
Gülüzar KARAKOÇ <karakoc.guluzar99@gmail.com>
2025-01-01 15:10:10 +0300

    Refactor : font added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './App/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-//reportWebVitals();
